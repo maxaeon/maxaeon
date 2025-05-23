ef342a2ea38acd90addcff99abf90a90aa1bf508
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2025-05-23 00:56:10 -0400

    Add captions and update gallery script

--- scripts/generate-art-manifest.js
+++ scripts/generate-art-manifest.js
@@ -2,11 +2,34 @@ const fs = require('fs');
 const path = require('path');
 
 const imageDir = path.join(__dirname, '../art/images');
-const outFile = path.join(__dirname, '../art/images.json');
+const imagesFile = path.join(__dirname, '../art/images.json');
+const captionsFile = path.join(__dirname, '../art/captions.json');
+
+const formatCaption = name =>
+  name.replace(/\.[^/.]+$/, '')
+      .replace(/[-_]+/g, ' ')
+      .trim();
 
 const files = fs.readdirSync(imageDir)
   .filter(f => /\.(png|jpg|jpeg|gif|webp)$/i.test(f))
   .sort();
 
-fs.writeFileSync(outFile, JSON.stringify(files, null, 2));
+let captions = {};
+if (fs.existsSync(captionsFile)) {
+  try {
+    captions = JSON.parse(fs.readFileSync(captionsFile, 'utf8'));
+  } catch (err) {
+    console.error('Could not parse captions.json, starting fresh');
+  }
+}
+
+const newCaptions = {};
+files.forEach(f => {
+  newCaptions[f] = captions[f] || formatCaption(f);
+});
+
+fs.writeFileSync(imagesFile, JSON.stringify(files, null, 2));
+fs.writeFileSync(captionsFile, JSON.stringify(newCaptions, null, 2));
+
 console.log(`Wrote ${files.length} files to images.json`);
+console.log(`Updated captions.json with ${Object.keys(newCaptions).length} entries`);
